b1c25b59207ce77cd8dd767161c112fa0cd373ec
Jonathan Alpart <panjialang@gmail.com>
2020-05-16 17:34:28 -0500

    defining and exporting employee classes

--- Develop/app.js
+++ Develop/app.js
@@ -127,23 +127,6 @@ function Employee(name, type, email, id) {
     // };
 }
 
-// class Employee extends Intern {
-//     constructor(school) {
-
-//         super(teamMemberName, employeeType);
-//         this.school = school;
-//     }
-// }
-
-// class Employee extends Engineer {
-//     constructor(gitHub) {
-
-//         super(teamMemberName, employeeType);
-//         this.gitHub = gitHub;
-//     }
-// }
-
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,10 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require('./employee');
+
+class Employee extends Intern {
+    constructor(school) {
+
+        super(name, type, email, id);
+        this.school = school;
+    }
+}
