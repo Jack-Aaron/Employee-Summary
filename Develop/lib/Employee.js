74cc0631befc82f15809e2e97a835801c2a02a5e
Jonathan Alpart <panjialang@gmail.com>
2020-05-16 21:05:25 -0500

    manager.js passes all tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,23 +5,13 @@ class Employee {
         this.id = id;
         this.email = email;
     }
+    getName() { return this.name }
 
-    getName() {
-        return this.name;
-    }
-    
-    getId() {
-        return this.id;
-        
-    }
-    getEmail() {
-        return this.email;
-    }
+    getId() { return this.id }
 
-    getRole() {
-        return 'Employee';
-    }
+    getEmail() { return this.email }
 
+    getRole() { return 'Employee' }
 }
 
 module.exports = Employee;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -3,8 +3,12 @@ const Employee = require('./employee');
 
 class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
-
         super(name, id, email);
         this.officeNumber = officeNumber;
     }
+    getOfficeNumber() { return this.officeNumber }
+
+    getRole() { return 'Manager' }
 }
+
+module.exports = Manager
